1827bdf8576a427d3b1807076b94ea2d82ae1936
Katherin Gomez ⚡ <70305826+katgzco@users.noreply.github.com>
2021-08-28 15:50:21 -0500

    Add sudo

--- config_script.sh
+++ config_script.sh
@@ -53,7 +53,7 @@ Holberton School
 END
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sed -i '57 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '57 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 #restart nginx
 sudo service nginx restart
 
